54eb79cf73c3dcaa041413ea16f963744c83b5c8
Farhan Kalam <farhanjune2004@gmail.com>
2026-09-13 22:18:21 -0400

    Name every branch under its line

--- backend/lines.py
+++ backend/lines.py
@@ -1,36 +1,37 @@
-"""Name every rail line once."""
+"""Name every rail line and branch once."""
+
+# What each branch is called
+BRANCHES: dict[str, dict[str, str]] = {
+    "Green": {
+        "Green-B": "B",
+        "Green-C": "C",
+        "Green-D": "D",
+        "Green-E": "E",
+    },
+    "CR": {
+        "CR-Fairmount": "Fairmount",
+        "CR-Fitchburg": "Fitchburg",
+        "CR-Foxboro": "Foxboro",
+        "CR-Franklin": "Franklin",
+        "CR-Greenbush": "Greenbush",
+        "CR-Haverhill": "Haverhill",
+        "CR-Kingston": "Kingston",
+        "CR-Lowell": "Lowell",
+        "CR-Needham": "Needham",
+        "CR-NewBedford": "New Bedford",
+        "CR-Newburyport": "Newburyport",
+        "CR-Providence": "Providence",
+        "CR-Worcester": "Worcester",
+    },
+}
 
 LINES: list[tuple[str, str, str, tuple[str, ...]]] = [
     ("Red", "RED", "Red Line", ("Red",)),
     ("Mattapan", "MATTAPAN", "Mattapan Line", ("Mattapan",)),
     ("Orange", "ORANGE", "Orange Line", ("Orange",)),
-    (
-        "Green",
-        "GREEN",
-        "Green Line",
-        ("Green-B", "Green-C", "Green-D", "Green-E"),
-    ),
+    ("Green", "GREEN", "Green Line", tuple(BRANCHES["Green"])),
     ("Blue", "BLUE", "Blue Line", ("Blue",)),
-    (
-        "CR",
-        "COMMUTER",
-        "Commuter Rail",
-        (
-            "CR-Fairmount",
-            "CR-Fitchburg",
-            "CR-Foxboro",
-            "CR-Franklin",
-            "CR-Greenbush",
-            "CR-Haverhill",
-            "CR-Kingston",
-            "CR-Lowell",
-            "CR-Needham",
-            "CR-NewBedford",
-            "CR-Newburyport",
-            "CR-Providence",
-            "CR-Worcester",
-        ),
-    ),
+    ("CR", "COMMUTER", "Commuter Rail", tuple(BRANCHES["CR"])),
 ]
 
 # The line each route is drawn under
@@ -38,3 +39,8 @@ LINE_OF: dict[str, str] = {}
 for line_id, _, _, routes in LINES:
     for route in routes:
         LINE_OF[route] = line_id
+
+# Every route that is a branch of a line
+BRANCH_ROUTES: set[str] = set()
+for branches in BRANCHES.values():
+    BRANCH_ROUTES.update(branches)
